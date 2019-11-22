b96f4bad49c2c164c21686549da5ad34d05342e7
Landsfadern <elias.w.burstrom@gmail.com>
2019-11-22 08:39:38 +0100

    please work

--- Simulation.py
+++ Simulation.py
@@ -1,18 +1,32 @@
 import numpy as np
+import random
 
-def PlotGraph(edges,nodes):    
+nNodes = 10
+
+
+cityPositions = np.array()
+cityMap = np.random.randint(0, high=2, size=(nNodes, nNodes))
+
+cityMap = np.triu(cityMap, k=1)
+print(cityMap)
+
+
+def PlotGraph(edges, nodes):
     indecesOfEdges = np.where(edges == 1)
-    fromCityPositions = [nodes[indecesOfEdges[0],0],nodes[indecesOfEdges[0],1]]
-    toCityPositions = [nodes[indecesOfEdges[1],0],nodes[indecesOfEdges[1],1]]
-    
+    fromCityPositions = [nodes[indecesOfEdges[0], 0],
+                         nodes[indecesOfEdges[0], 1]]
+    toCityPositions = [nodes[indecesOfEdges[1], 0],
+                       nodes[indecesOfEdges[1], 1]]
+
     plt.figure()
     plt.gca().set_aspect('equal', adjustable='box')
     markerSize = 10
     fontSize = 30
     lineWidth = 1
-    plt.xlabel('x',fontsize=fontSize)
-    plt.ylabel('y',fontsize=fontSize)
+    plt.xlabel('x', fontsize=fontSize)
+    plt.ylabel('y', fontsize=fontSize)
     plt.tick_params(axis='both', labelsize=fontSize)
-    
-    plt.plot([fromCityPositions[0][:],toCityPositions[0][:]],[fromCityPositions[1][:],toCityPositions[1][:]],'k',linewidth=lineWidth)
-    plt.plot(nodes[:,0],nodes[:,1],'or',markersize=markerSize)
+
+    plt.plot([fromCityPositions[0][:], toCityPositions[0][:]], [
+             fromCityPositions[1][:], toCityPositions[1][:]], 'k', linewidth=lineWidth)
+    plt.plot(nodes[:, 0], nodes[:, 1], 'or', markersize=markerSize)
