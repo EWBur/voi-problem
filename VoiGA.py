34e75369672d54bed1144c4e1728ccc725843080
Jesper <jespjon@student.chalmers.se>
2019-11-23 10:18:38 +0100

    Update VoiGA.py

--- VoiGA.py
+++ VoiGA.py
@@ -1,23 +1,27 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 def InitializePopulation(nCities,populationSize):
     population = np.random.rand(populationSize,nCities)
     return population
 
-def DecodePopulation(nVois,population):
+def DecodePopulation(nVois,population,zeroThreshold):
     nCities = np.size(population,1)
+    
     decodedPopulation = np.round(population/(np.tile(np.sum(population,1),(nCities,1))).T*nVois)
     
     ### FIX ###
     for i in range(populationSize):
         nVoisInCity = np.sum(decodedPopulation,1)[i]
-        while np.sum(decodedPopulation,1)[i] != nVois:
+        while int(nVoisInCity) != int(nVois):
             decodedPopulation[i,np.random.randint(0,nCities)] += np.sign(nVois-nVoisInCity)
+            decodedPopulation = np.maximum(decodedPopulation,0)
+            nVoisInCity = np.sum(decodedPopulation,1)[i]
     
     return decodedPopulation
 
-def FitnessOfPopulation(population):
-    populationFitness = np.linspace(0,100,np.size(population,0))
+def FitnessOfPopulation(decodedPopulation):
+    populationFitness = np.sum(decodedPopulation[:,0:5],1)
     return populationFitness
 
 def TournamentSelection(populationFitness,tournamentSize,tournamentProbability):
@@ -29,15 +33,15 @@ def TournamentSelection(populationFitness,tournamentSize,tournamentProbability):
     probabilities = np.heaviside(-(np.random.rand(tournamentSize) - tournamentProbability),0)
     probabilities[tournamentSize-1] = 1
     possibleIndeces = np.nonzero(probabilities)
-    chosenIndex = sortingIndeces[possibleIndeces[0][0]]
+    chosenIndex = tournamentIndeces[sortingIndeces[possibleIndeces[0][0]]]
     return chosenIndex
 
 def Crossover(chromosome1,chromosome2):
     chromosomeLength = len(chromosome1)
     crossoverPoint = np.random.randint(0,chromosomeLength)
-    
-    chromosome1New = chromosome1[0:crossoverPoint] + chromosome2[crossoverPoint:-1]
-    chromosome2New = chromosome2[0:crossoverPoint] + chromosome1[crossoverPoint:-1]
+
+    chromosome1New = np.concatenate((chromosome1[0:crossoverPoint],chromosome2[crossoverPoint:chromosomeLength]))
+    chromosome2New = np.concatenate((chromosome2[0:crossoverPoint],chromosome1[crossoverPoint:chromosomeLength]))
     return chromosome1New,chromosome2New
 
 def Mutation(chromosome,mutationProbability, creepRate):
@@ -46,7 +50,7 @@ def Mutation(chromosome,mutationProbability, creepRate):
     for iGene in range(len(indecesToMutate)):
         currentGene = chromosome[iGene]
         mutatedGene = currentGene - creepRate/2 + creepRate*np.random.rand()
-        mutatedGene = np.min(np.max(0,mutatedGene),1)
+        mutatedGene = min(max(0,mutatedGene),1)
         chromosome[iGene] = mutatedGene
     
     return chromosome
@@ -67,17 +71,49 @@ def Mutation(chromosome,mutationProbability, creepRate):
         cityMap[indexOfMinNeighbour[i],i] = 1
     '''
 
-nCities = 10
-populationSize = 5
-nVois = 50
-tournamentSize = 5
+nCities = 20
+populationSize = 30
+nVois = 100
+tournamentSize = 2
 tournamentProbability = 0.7
-mutationProbability = 0.5
+mutationProbability = 1/nCities
 creepRate = 0.1
+noTimeSteps = 1
+elitismNumber = 1
+zeroThreshold = 0.15
 
 population = InitializePopulation(nCities,populationSize)
-decodedPopulation = DecodePopulation(nVois,population)
-populationFitness = FitnessOfPopulation(decodedPopulation)
-selectedIndecex = TournamentSelection(populationFitness,tournamentSize,tournamentProbability)
-chromosome1New,chromosome2New = Crossover([0,1,2,3,4,5],[6,7,8,9])
-Mutation(chromosome1New,mutationProbability,creepRate)
+
+greatestFitness = np.zeros(noTimeSteps+1)
+for iTime in range(noTimeSteps):
+    decodedPopulation = DecodePopulation(nVois,population,zeroThreshold)
+    populationFitness = FitnessOfPopulation(decodedPopulation)
+    
+    generationGreatestFitness = np.max(populationFitness)
+    if generationGreatestFitness > greatestFitness[iTime]:
+        greatestFitness[iTime+1] = generationGreatestFitness
+        bestChromosome = population[np.argmax(populationFitness),:]
+    else:
+        greatestFitness[iTime+1] = greatestFitness[iTime]
+    
+    newPopulation = np.zeros((populationSize,nCities))
+    for jChromosomePair in range(int(populationSize/2)):
+        chosenIndex1 = TournamentSelection(populationFitness,tournamentSize,tournamentProbability)
+        chosenIndex2 = TournamentSelection(populationFitness,tournamentSize,tournamentProbability)
+        
+        chromosome1 = population[chosenIndex1,:]
+        chromosome2 = population[chosenIndex2,:]
+        
+        chromosome1,chromosome2 = Crossover(chromosome1,chromosome2)
+        
+        chromosome1 = Mutation(chromosome1,mutationProbability,creepRate)
+        chromosome2 = Mutation(chromosome2,mutationProbability,creepRate)
+        
+        newPopulation[2*jChromosomePair,:] = chromosome1
+        newPopulation[2*jChromosomePair+1,:] = chromosome2
+            
+    newPopulation[0:elitismNumber,:] = bestChromosome  
+    population = newPopulation
+    
+plt.plot(np.linspace(0,noTimeSteps,noTimeSteps+1),greatestFitness)
+    
